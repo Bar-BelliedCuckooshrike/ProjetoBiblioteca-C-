5c4843d7867f169b7bb3ce3f6e75b498c432c720
Felipe Augusto <felipe_augusto08@hotmail.com>
2020-09-09 16:15:13 -0300

    Primeiro commit: Projeto criado

---
+++ ProjetoBiblioteca/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProjetoBiblioteca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----MINHA BIBLIOTECA-----\n");
+            Console.WriteLine("Selecione uma opção: \n");
+            Console.WriteLine("[1] - Cadastrar um livro \n");
+            Console.WriteLine("[2] - Listar livros");
+            Console.WriteLine("[3] - Editar estante");
+            Console.WriteLine("[4] - Editar livros");
+        }
+    }
+}
