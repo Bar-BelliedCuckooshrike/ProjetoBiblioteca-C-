c9560ee4ca2b3764651dfe2a17faf16156ddcbed
Felipe Augusto <felipe_augusto08@hotmail.com>
2020-09-09 16:24:20 -0300

    Segundo commit: menu

--- ProjetoBiblioteca/Program.cs
+++ ProjetoBiblioteca/Program.cs
@@ -6,12 +6,32 @@ namespace ProjetoBiblioteca
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("-----MINHA BIBLIOTECA-----\n");
-            Console.WriteLine("Selecione uma opção: \n");
-            Console.WriteLine("[1] - Cadastrar um livro \n");
-            Console.WriteLine("[2] - Listar livros");
-            Console.WriteLine("[3] - Editar estante");
-            Console.WriteLine("[4] - Editar livros");
+            int op;
+
+            do
+            {
+                Console.WriteLine("-----MINHA BIBLIOTECA-----\n");
+                Console.WriteLine("Selecione uma opção: \n");
+                Console.WriteLine("[1] - Fazer login");
+                Console.WriteLine("[0] - Sair");
+                op = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+                switch (op)
+                {
+                    case 1:
+                        Console.WriteLine("[1] - Cadastrar um livro");
+                        Console.WriteLine("[2] - Listar livros");
+                        Console.WriteLine("[3] - Editar estante");
+                        Console.WriteLine("[4] - Editar livros");
+                        break;
+
+                    case 0:
+                        Console.WriteLine("Saindo...");
+                        break;
+                }
+            } while (op != 0);
+
         }
     }
 }
